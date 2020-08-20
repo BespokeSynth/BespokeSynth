6a9f6fdbdf2c5efa506844bfd12d7f18a941ad1d
Ryan Challinor Ubuntu <awwbees@gmail.com>
2020-08-20 10:34:37 -0400

    shell script to install all dependencies needed for building bespoke in linux with juce 5.4.7

---
+++ Builds/LinuxMakefile/build/bespoke_dependencies.sh
@@ -0,0 +1,18 @@
+#running this script to install these dependencies should allow you to both run and compile bespoke
+#getting all of these dependencies is definitely overkill if you only want to run the binary, and don't care about compiling, but I'm not good enough at linux to narrow it down for you
+sudo apt-get -y install g++
+sudo apt-get -y install libfreetype6-dev
+sudo apt-get -y install libx11-dev
+sudo apt-get -y install libxinerama-dev
+sudo apt-get -y install libxrandr-dev
+sudo apt-get -y install libxcursor-dev
+sudo apt-get -y install mesa-common-dev
+sudo apt-get -y install libasound2-dev
+sudo apt-get -y install freeglut3-dev
+sudo apt-get -y install libxcomposite-dev
+sudo apt-get -y install libcurl4
+sudo apt-get -y install libusb-1.0-0-dev
+sudo apt-get -y install libgtk-3-dev
+sudo apt-get -y install python-dev
+sudo apt-get -y install libcurl4-openssl-dev
+sudo apt-get -y install libwebkit2gtk-4.0-dev
