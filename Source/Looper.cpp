BespokeSynth/BespokeSynth

commit 4919af31ac46891dca7a76e4cae7b117852244a8

massive refactor to how transport scheduling works, to allow events to trigger early so scripts can schedule stuff on time. probably causes a bunch of bugs in various places when "do event lookahead (exp.)" is turned on. it automatically gets turned on when you create a ScriptModule. in the process of making this change, also fixed a bunch of issues where things weren't getting scheduled sample-accurately. notable issue when "event lookahead" is on: rapid retriggering of the same pitch to synths and drumplayer doesn't work properly, need to fix that up next.

Source/Looper.cpp
@@ -301,7 +301,7 @@ void Looper::Process(double time)
    float oldLoopPos = mLoopPos;
    int sampsPerBar = mLoopLength / mNumBars;
    if (!mPausePos)
-      mLoopPos = sampsPerBar * ((TheTransport->GetMeasure() % mNumBars) + TheTransport->GetMeasurePos());
+      mLoopPos = sampsPerBar * ((TheTransport->GetMeasure(time) % mNumBars) + TheTransport->GetMeasurePos(time));
    
    if (oldLoopPos > mLoopLength - bufferSize * mSpeed - 1 && mLoopPos < oldLoopPos)
    {
@@ -362,10 +362,10 @@ void Looper::Process(double time)
          ProcessScratch();
       
       if (mFourTet > 0)
-         ProcessFourTet(i);
+         ProcessFourTet(time, i);
       
       if (mBeatwheel)
-         ProcessBeatwheel(i);
+         ProcessBeatwheel(time, i);
       
       float offset = mLoopPos+i*mSpeed+mLoopPosOffset+latencyOffset;
       float output[ChannelBuffer::kMaxNumChannels];
@@ -514,10 +514,10 @@ void Looper::ProcessScratch()
    FloatWrap(mLoopPosOffset, mLoopLength);
 }
 
-void Looper::ProcessFourTet(int sampleIdx)
+void Looper::ProcessFourTet(double time, int sampleIdx)
 {
-   float measurePos = TheTransport->GetMeasurePos() + sampleIdx/(TheTransport->MsPerBar() / gInvSampleRateMs);
-   measurePos += TheTransport->GetMeasure() % mNumBars;
+   float measurePos = TheTransport->GetMeasurePos(time) + sampleIdx/(TheTransport->MsPerBar() / gInvSampleRateMs);
+   measurePos += TheTransport->GetMeasure(time) % mNumBars;
    measurePos /= mNumBars;
    int numSlices = mFourTetSlices * 2 * mNumBars;
    measurePos *= numSlices;
@@ -534,7 +534,7 @@ void Looper::ProcessFourTet(int sampleIdx)
    FloatWrap(mLoopPosOffset, mLoopLength);
 }
 
-void Looper::ProcessBeatwheel(int sampleIdx)
+void Looper::ProcessBeatwheel(double time, int sampleIdx)
 {
    bool bothHeld = false;
    bool noneHeld = false;
@@ -565,8 +565,8 @@ void Looper::ProcessBeatwheel(int sampleIdx)
    if (noneHeld)
       depthLevel = 2;
    int slicesPerBar = TheTransport->GetTimeSigTop() * (1 << depthLevel);
-   int lastSlice = GetMeasureSliceIndex(sampleIdx-1, slicesPerBar);
-   int slice = GetMeasureSliceIndex(sampleIdx, slicesPerBar);
+   int lastSlice = GetMeasureSliceIndex(time, sampleIdx-1, slicesPerBar);
+   int slice = GetMeasureSliceIndex(time, sampleIdx, slicesPerBar);
    int numSlices = slicesPerBar*mNumBars;
    int loopLength = mLoopLength;
    if (mBeatwheelSingleMeasure)
@@ -618,10 +618,10 @@ float Looper::GetActualLoopPos(int samplesIn) const
    return pos;
 }
 
-int Looper::GetMeasureSliceIndex(int sampleIdx, int slicesPerBar)
+int Looper::GetMeasureSliceIndex(double time, int sampleIdx, int slicesPerBar)
 {
-   float measurePos = TheTransport->GetMeasurePos() + sampleIdx/(TheTransport->MsPerBar() / gInvSampleRateMs);
-   measurePos += TheTransport->GetMeasure() % mNumBars;
+   float measurePos = TheTransport->GetMeasurePos(time) + sampleIdx/(TheTransport->MsPerBar() / gInvSampleRateMs);
+   measurePos += TheTransport->GetMeasure(time) % mNumBars;
    measurePos /= mNumBars;
    int numSlices = slicesPerBar * mNumBars;
    measurePos *= numSlices;
@@ -727,7 +727,7 @@ void Looper::DrawModule()
       if (mWriteInput)
          ofSetColor(255, 0, 0, 150);
       else
-         ofSetColor(255, 100, 0, 100 + 50 * (cosf(TheTransport->GetMeasurePos() * 4 * FTWO_PI)));
+         ofSetColor(255, 100, 0, 100 + 50 * (cosf(TheTransport->GetMeasurePos(gTime) * 4 * FTWO_PI)));
       ofRect(mQueueCaptureButton->GetRect(true));
       ofPopStyle();
    }
@@ -937,7 +937,7 @@ void Looper::UpdateNumBars(int oldNumBars)
    SetLoopLength(MIN(sampsPerBar * mNumBars, MAX_BUFFER_SIZE-1));
    while (mLoopPos > sampsPerBar)
       mLoopPos -= sampsPerBar;
-   mLoopPos += sampsPerBar * (TheTransport->GetMeasure() % mNumBars);
+   mLoopPos += sampsPerBar * (TheTransport->GetMeasure(gTime) % mNumBars);
    if (oldNumBars < mNumBars)
    {
       int oldLoopLength = abs(int(TheTransport->MsPerBar() * oldNumBars / 1000 * gSampleRate));

commit 3e45792a667156891fb74666e6594314f871c969

fix looper behavior that was broken by refactor in 4919af31ac46891dca7a76e4cae7b117852244a8

Source/Looper.cpp
@@ -349,6 +349,7 @@ void Looper::Process(double time)
    if (mPitchShift != 1)
       latencyOffset = mPitchShifter[0]->GetLatency();
 
+   double processStartTime = gTime;
    for (int i=0; i<bufferSize; ++i)
    {
       float smooth = .001f;
@@ -361,17 +362,17 @@ void Looper::Process(double time)
          ProcessScratch();
       
       if (mFourTet > 0)
-         ProcessFourTet(time, i);
+         ProcessFourTet(processStartTime, i);
       
       if (mBeatwheel)
-         ProcessBeatwheel(time, i);
+         ProcessBeatwheel(processStartTime, i);
       
       float offset = mLoopPos+i*mSpeed+mLoopPosOffset+latencyOffset;
       float output[ChannelBuffer::kMaxNumChannels];
       ::Clear(output, ChannelBuffer::kMaxNumChannels);
       
       if (mGranular)
-         ProcessGranular(time, offset, output);
+         ProcessGranular(processStartTime, offset, output);
       
       for (int ch=0; ch<mBuffer->NumActiveChannels(); ++ch)
       {
@@ -523,9 +524,9 @@ void Looper::ProcessFourTet(double time, int sampleIdx)
    int slice = (int)measurePos;
    float sliceProgress = measurePos - slice;
    if (slice % 2 == 0)
-      mLoopPosOffset = (sliceProgress + slice/2) * (mLoopLength/numSlices * 2);
+      mLoopPosOffset = (sliceProgress + slice/2) * (mLoopLength/float(numSlices) * 2);
    else
-      mLoopPosOffset = (1 - sliceProgress + slice/2) * (mLoopLength/numSlices * 2);
+      mLoopPosOffset = (1 - sliceProgress + slice/2) * (mLoopLength/float(numSlices) * 2);
    
    //offset regular movement
    mLoopPosOffset -= mLoopPos+sampleIdx*mSpeed;