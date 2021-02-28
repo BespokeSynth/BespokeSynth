edef0e77c84749da1b4f5d1e5bae9a383d046bd6
Ryan Challinor Windows <awwbees@gmail.com>
2021-02-27 23:56:25 -0500

    boilerplate script to show how to receive midi messages directly via script

---
+++ Builds/MacOSX/build/Release/data/scripts/midicontroller_input.py
@@ -0,0 +1,8 @@
+import midicontroller
+
+controller = midicontroller.get("midicontroller")
+controller.add_script_listener(me.me())
+
+def on_midi(messageType, control, value, channel):
+   debug = str(messageType) + " " + str(control) + " " + str(value) + " " + str(channel)
+   me.output(debug)
