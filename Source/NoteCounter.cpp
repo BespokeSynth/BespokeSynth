d6515d7b36a0b000950108e42e8de0bd0112a3e8
Ryan Challinor <awwbees@gmail.com>
2021-04-24 23:02:03 -0400

    notecounter module, to advance sequentially through notes. useful to use for driving a sequencer

--- Source/NoteCounter.cpp
+++ Source/NoteCounter.cpp
@@ -9,3 +9,123 @@
 */
 
 #include "NoteCounter.h"
+#include "IAudioSource.h"
+#include "SynthGlobals.h"
+#include "DrumPlayer.h"
+#include "ModularSynth.h"
+#include "UIControlMacros.h"
+
+NoteCounter::NoteCounter()
+: mInterval(kInterval_16n)
+, mIntervalSelector(nullptr)
+, mStart(0)
+, mLength(16)
+, mStep(0)
+, mCustomDivisor(8)
+{
+   mTransportListenerInfo = TheTransport->AddListener(this, mInterval, OffsetInfo(0, true), true);
+}
+
+void NoteCounter::CreateUIControls()
+{
+   IDrawableModule::CreateUIControls();
+   
+   UIBLOCK0();
+   DROPDOWN(mIntervalSelector, "interval", ((int*)(&mInterval)), 50); UIBLOCK_SHIFTRIGHT();
+   CHECKBOX(mSyncCheckbox, "sync", &mSync); UIBLOCK_NEWLINE();
+   INTSLIDER(mStartSlider, "start", &mStart, 0, 32);
+   INTSLIDER(mLengthSlider, "length", &mLength, 1, 32);
+   INTSLIDER(mCustomDivisorSlider, "div", &mCustomDivisor, 1, 32);
+   ENDUIBLOCK(mWidth, mHeight);
+   
+   mIntervalSelector->AddLabel("1n", kInterval_1n);
+   mIntervalSelector->AddLabel("2n", kInterval_2n);
+   mIntervalSelector->AddLabel("4n", kInterval_4n);
+   mIntervalSelector->AddLabel("4nt", kInterval_4nt);
+   mIntervalSelector->AddLabel("8n", kInterval_8n);
+   mIntervalSelector->AddLabel("8nt", kInterval_8nt);
+   mIntervalSelector->AddLabel("16n", kInterval_16n);
+   mIntervalSelector->AddLabel("16nt", kInterval_16nt);
+   mIntervalSelector->AddLabel("32n", kInterval_32n);
+   mIntervalSelector->AddLabel("64n", kInterval_64n);
+   mIntervalSelector->AddLabel("div", kInterval_CustomDivisor);
+}
+
+NoteCounter::~NoteCounter()
+{
+   TheTransport->RemoveListener(this);
+}
+
+void NoteCounter::DrawModule()
+{
+   if (Minimized() || IsVisible() == false)
+      return;
+   mIntervalSelector->Draw();
+   mSyncCheckbox->Draw();
+   mStartSlider->Draw();
+   mLengthSlider->Draw();
+   mCustomDivisorSlider->SetShowing(mInterval == kInterval_CustomDivisor);
+   mCustomDivisorSlider->Draw();
+}
+
+void NoteCounter::OnTimeEvent(double time)
+{
+   if (!mEnabled)
+      return;
+   
+   if (mSync)
+   {
+      int stepsPerMeasure = TheTransport->GetStepsPerMeasure(this);
+      int measure = TheTransport->GetMeasure(time);
+      mStep = (TheTransport->GetQuantized(time, mTransportListenerInfo) + measure * stepsPerMeasure) % mLength;
+   }
+   else
+   {
+      mStep = (mStep + 1) % mLength;
+   }
+   
+   mNoteOutput.Flush(time);
+   PlayNoteOutput(time, mStep + mStart, 127, -1);
+}
+
+void NoteCounter::CheckboxUpdated(Checkbox* checkbox)
+{
+   if (checkbox == mEnabledCheckbox)
+      mNoteOutput.Flush(gTime);
+}
+
+void NoteCounter::IntSliderUpdated(IntSlider* slider, int oldVal)
+{
+   if (slider == mCustomDivisorSlider)
+      mTransportListenerInfo->mCustomDivisor = mCustomDivisor;
+}
+
+void NoteCounter::DropdownUpdated(DropdownList* list, int oldVal)
+{
+   if (list == mIntervalSelector)
+   {
+      TransportListenerInfo* transportListenerInfo = TheTransport->GetListenerInfo(this);
+      if (transportListenerInfo != nullptr)
+         transportListenerInfo->mInterval = mInterval;
+   }
+}
+
+void NoteCounter::GetModuleDimensions(float& width, float& height)
+{
+   width = mWidth;
+   height = mHeight;
+   if (!mCustomDivisorSlider->IsShowing())
+      height -= 17;
+}
+
+void NoteCounter::LoadLayout(const ofxJSONElement& moduleInfo)
+{
+   mModuleSaveData.LoadString("target", moduleInfo);
+   
+   SetUpFromSaveData();
+}
+
+void NoteCounter::SetUpFromSaveData()
+{
+   SetUpPatchCables(mModuleSaveData.GetString("target"));
+}
--- Source/NoteCounter.h
+++ Source/NoteCounter.h
@@ -9,3 +9,53 @@
 */
 
 #pragma once
+#include "IDrawableModule.h"
+#include "INoteSource.h"
+#include "Transport.h"
+#include "Slider.h"
+#include "DropdownList.h"
+
+class NoteCounter : public IDrawableModule, public INoteSource, public ITimeListener, public IIntSliderListener, public IDropdownListener
+{
+public:
+   NoteCounter();
+   ~NoteCounter();
+   static IDrawableModule* Create() { return new NoteCounter(); }
+   
+   string GetTitleLabel() override { return "note counter"; }
+   void CreateUIControls() override;
+   
+   void SetEnabled(bool enabled) override { mEnabled = enabled; }
+   
+   //ITimeListener
+   void OnTimeEvent(double time) override;
+   
+   void CheckboxUpdated(Checkbox* checkbox) override;
+   void IntSliderUpdated(IntSlider* slider, int oldVal) override;
+   void DropdownUpdated(DropdownList* list, int oldVal) override;
+   
+   virtual void LoadLayout(const ofxJSONElement& moduleInfo) override;
+   virtual void SetUpFromSaveData() override;
+   
+private:
+   //IDrawableModule
+   void DrawModule() override;
+   void GetModuleDimensions(float& width, float& height) override;
+   bool Enabled() const override { return mEnabled; }
+   
+   float mWidth;
+   float mHeight;
+   NoteInterval mInterval;
+   DropdownList* mIntervalSelector;
+   int mStart;
+   IntSlider* mStartSlider;
+   int mLength;
+   IntSlider* mLengthSlider;
+   int mStep;
+   Checkbox* mSyncCheckbox;
+   bool mSync;
+   int mCustomDivisor;
+   IntSlider* mCustomDivisorSlider;
+   
+   TransportListenerInfo* mTransportListenerInfo;
+};
