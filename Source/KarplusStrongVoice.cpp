3b7c13c491155644047827486964c6837ccd49f0
Ryan Challinor Windows <awwbees@gmail.com>
2021-06-08 21:41:01 -0400

    update karplus strong filtering rate to sound the same across the pitch range

--- Source/KarplusStrongVoice.cpp
+++ Source/KarplusStrongVoice.cpp
@@ -69,9 +69,11 @@ bool KarplusStrongVoice::Process(double time, ChannelBuffer* out, int oversampli
          mOwner->ComputeSliders(pos/oversampling);
       
       float pitch = GetPitch(pos/oversampling);
+      if (mVoiceParams->mInvert)
+         pitch += 12;   //inverting the pitch gives an octave down sound by halving the resonating frequency, so correct for that
       
       float freq = TheScale->PitchToFreq(pitch);
-      float filterRate = ofMap(freq, 0, 880, mVoiceParams->mFilter, 0);
+      float filterRate = mVoiceParams->mFilter * freq/300;
       float filterLerp = ofClamp(exp2(-filterRate / oversampling), 0, 1);
       
       float oscPhaseInc = GetPhaseInc(mVoiceParams->mExciterFreq) / oversampling;
