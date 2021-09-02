3afda09e7dd9b12470cc370786debe9a8bb8a25f
Ryan Challinor <awwbees@gmail.com>
2021-09-02 15:22:59 -0400

    move overlapping ui control on drumsequencer

--- Source/StepSequencer.cpp
+++ Source/StepSequencer.cpp
@@ -91,7 +91,7 @@ void StepSequencer::CreateUIControls()
    mRandomizationAmountSlider = new FloatSlider(this, "r amt", mRandomizationDensitySlider, kAnchor_Right, 65, 15, &mRandomizationAmount, 0, 1, 2);
    mNumMeasuresSlider = new IntSlider(this, "measures", 5, 22, 80, 15, &mNumMeasures, 1, 4);
    mPresetDropdown = new DropdownList(this,"preset",5,4,&mPreset);
-   mGridYOffDropdown = new DropdownList(this,"yoff",240,20,&mGridYOff);
+   mGridYOffDropdown = new DropdownList(this,"yoff",275,4,&mGridYOff);
    mAdjustOffsetsCheckbox = new Checkbox(this,"offsets",175,4,&mAdjustOffsets);
    mRepeatRateDropdown = new DropdownList(this,"repeat",155,22,(int*)(&mRepeatRate));
    mStepIntervalDropdown = new DropdownList(this,"step",133,4,(int*)(&mStepInterval));
