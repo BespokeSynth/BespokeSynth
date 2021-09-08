db4756aa7fe0add57c747d5fde3a5890596c2aaf
Ryan Challinor Windows <awwbees@gmail.com>
2021-09-07 23:37:12 -0400

    windows installer, using the wix toolkit

---
+++ bespoke_windows_installer/bespoke_installer_create.bat
@@ -0,0 +1,5 @@
+"C:\Program Files (x86)\WiX Toolset v3.11\bin\heat" dir ..\Builds\MacOSX\build\Release\resource -o HarvestedResourceDir.wxs -scom -frag -srd -sreg -gg -cg BespokeResourceDir -dr RESOURCE_DIR_REF
+"C:\Program Files (x86)\WiX Toolset v3.11\bin\candle" BespokeSynth.wxs HarvestedResourceDir.wxs
+"C:\Program Files (x86)\WiX Toolset v3.11\bin\light" BespokeSynth.wixobj HarvestedResourceDir.wixobj -b ..\Builds\MacOSX\build\Release\resource -out BespokeSynth.msi -ext WixUIExtension
+
+@pause
