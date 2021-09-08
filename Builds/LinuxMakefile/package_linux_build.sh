dc8dd566b0951a49bc49a3c785bb28f7b3ca2125
Ryan Challinor Ubuntu <awwbees@gmail.com>
2021-09-08 12:34:50 -0400

    add script to package linux builds into a zip

---
+++ Builds/LinuxMakefile/package_linux_build.sh
@@ -0,0 +1,10 @@
+rm -f Bespoke-Linux.zip
+rm -rf BespokeSynth/
+mkdir BespokeSynth
+cp build/BespokeSynth BespokeSynth/BespokeSynth
+cp Bespoke-GLSLfix.sh BespokeSynth/Bespoke-GLSLfix.sh
+cp bespoke_dependencies.sh BespokeSynth/bespoke_dependencies.sh
+cp readme.txt BespokeSynth/readme.txt
+cp -r ../MacOSX/build/Release/resource BespokeSynth/resource
+zip -r Bespoke-Linux.zip BespokeSynth
+rm -rf BespokeSynth/
