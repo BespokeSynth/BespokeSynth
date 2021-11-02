4a423d1881061927c8c0652fd688f2595d33c9b4
Ryan Challinor <awwbees@gmail.com>
2021-11-01 22:32:52 -0400

    add "notetable" module
    this module translates notes with 0-32 pitch values into scale pitches

--- Source/NoteTable.h
+++ Source/NoteTable.h
@@ -16,15 +16,14 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 **/
 //
-//  NoteStepSequencer.h
+//  NoteTable.h
 //  modularSynth
 //
 //  Created by Ryan Challinor on 11/3/13.
 //
 //
 
-#ifndef __modularSynth__NoteStepSequencer__
-#define __modularSynth__NoteStepSequencer__
+#pragma once
 
 #include <iostream>
 #include "INoteReceiver.h"
@@ -37,62 +36,39 @@
 #include "INoteSource.h"
 #include "Slider.h"
 #include "UIGrid.h"
-#include "MidiController.h"
 #include "Scale.h"
-#include "IPulseReceiver.h"
 #include "GridController.h"
-#include "IDrivableSequencer.h"
-
-#define NSS_MAX_STEPS 32
 
 class PatchCableSource;
 
-class NoteStepSequencer : public IDrawableModule, public ITimeListener, public INoteSource, public IButtonListener, public IDropdownListener, public IIntSliderListener, public IFloatSliderListener, public MidiDeviceListener, public UIGridListener, public IAudioPoller, public IScaleListener, public INoteReceiver, public IPulseReceiver, public IGridControllerListener, public IDrivableSequencer
+class NoteTable : public IDrawableModule, public INoteSource, public IButtonListener, public IDropdownListener, public IIntSliderListener, public IFloatSliderListener, public UIGridListener, public IScaleListener, public INoteReceiver, public IGridControllerListener
 {
 public:
-   NoteStepSequencer();
-   virtual ~NoteStepSequencer();
-   static IDrawableModule* Create() { return new NoteStepSequencer(); }
-   
+   NoteTable();
+   virtual ~NoteTable();
+   static IDrawableModule* Create() { return new NoteTable(); }
    
    void CreateUIControls() override;
    
    void Init() override;
    void SetEnabled(bool enabled) override { mEnabled = enabled; }
-   
-   void SetMidiController(std::string name);
+   void Poll() override;
    
    UIGrid* GetGrid() const { return mGrid; }
    
    int RowToPitch(int row);
    int PitchToRow(int pitch);
-   void SetStep(int index, int pitch, int velocity, float length);
    
    //IDrawableModule
    bool IsResizable() const override { return true; }
    void Resize(float w, float h) override;
-   void Poll() override;
-
+   
    //IClickable
    void MouseReleased() override;
    bool MouseMoved(float x, float y) override;
-   bool MouseScrolled(int x, int y, float scrollX, float scrollY) override;
-   
-   //IAudioPoller
-   void OnTransportAdvanced(float amount) override;
-   
-   //ITimeListener
-   void OnTimeEvent(double time) override;
-   
-   //IPulseReceiver
-   void OnPulse(double time, float velocity, int flags) override;
    
    //IScaleListener
    void OnScaleChanged() override;
-   
-   //MidiDeviceListener
-   void OnMidiNote(MidiNote& note) override;
-   void OnMidiControl(MidiControl& control) override;
    
    //UIGridListener
    void GridUpdated(UIGrid* grid, int col, int row, float value, float oldValue) override;
@@ -104,17 +80,13 @@ public:
    //IGridControllerListener
    void OnControllerPageSelected() override;
    void OnGridButton(int x, int y, float velocity, IGridController* grid) override;
-
-   //IDrivableSequencer
-   bool HasExternalPulseSource() const override { return mHasExternalPulseSource; }
-   void ResetExternalPulseSource() override { mHasExternalPulseSource = false; }
    
    void ButtonClicked(ClickButton* button) override;
    void CheckboxUpdated(Checkbox* checkbox) override;
    void DropdownUpdated(DropdownList* list, int oldVal) override;
    void IntSliderUpdated(IntSlider* slider, int oldVal) override;
-   void FloatSliderUpdated(FloatSlider* slider, float oldVal) override;
-
+   void FloatSliderUpdated(FloatSlider* slider, float oldVal) override {}
+   
    void SaveLayout(ofxJSONElement& moduleInfo) override;
    void LoadLayout(const ofxJSONElement& moduleInfo) override;
    void SetUpFromSaveData() override;
@@ -129,18 +101,12 @@ private:
    void OnClicked(int x, int y, bool right) override;
    void UpdateGridControllerLights(bool force);
    
-   int ButtonToStep(int button);
-   int StepToButton(int step);
+   void PlayColumn(double time, int column, int velocity, int voiceIdx, ModulationParameters modulation);
+   void SetUpColumnControls();
    void SyncGridToSeq();
-   void UpdateLights();
-   void ShiftSteps(int amount);
-   void UpdateVelocityGridPos();
-   void SetUpStepControls();
    float ExtraWidth() const;
    float ExtraHeight() const;
    void RandomizePitches(bool fifths);
-   void Step(double time, float velocity, int pulseFlags);
-   void SendNoteToCable(int index, double time, int pitch, int velocity);
    
    enum NoteMode
    {
@@ -149,78 +115,36 @@ private:
       kNoteMode_Pentatonic,
       kNoteMode_Fifths
    };
-   
-   int mTones[NSS_MAX_STEPS];
-   int mVels[NSS_MAX_STEPS];
-   float mNoteLengths[NSS_MAX_STEPS];
-   
-   NoteInterval mInterval;
-   int mArpIndex;
-   
-   DropdownList* mIntervalSelector;
-   Checkbox* mRepeatIsHoldCheckbox;
+
    UIGrid* mGrid;
-   UIGrid* mVelocityGrid;
-   int mLastPitch;
-   int mLastVel;
-   int mLastStepIndex;
-   float mLastNoteLength;
-   double mLastNoteStartTime;
-   double mLastNoteEndTime;
-   bool mAlreadyDidNoteOff;
    int mOctave;
    IntSlider* mOctaveSlider;
    NoteMode mNoteMode;
    DropdownList* mNoteModeSelector;
-   IntSlider* mLoopResetPointSlider;
-   int mLoopResetPoint;
-   
    int mLength;
    IntSlider* mLengthSlider;
    bool mSetLength;
    int mNoteRange;
-   bool mShowStepControls;
+   bool mShowColumnControls;
    int mRowOffset;
    
-   MidiController* mController;
-
-   ClickButton* mShiftBackButton;
-   ClickButton* mShiftForwardButton;
-   ClickButton* mClearButton;
-   
    ClickButton* mRandomizePitchButton;
-   ClickButton* mRandomizeLengthButton;
-   ClickButton* mRandomizeVelocityButton;
    float mRandomizePitchChance;
    float mRandomizePitchRange;
-   float mRandomizeLengthChance;
-   float mRandomizeLengthRange;
-   float mRandomizeVelocityChance;
-   float mRandomizeVelocityDensity;
    FloatSlider* mRandomizePitchChanceSlider;
    FloatSlider* mRandomizePitchRangeSlider;
-   FloatSlider* mRandomizeLengthChanceSlider;
-   FloatSlider* mRandomizeLengthRangeSlider;
-   FloatSlider* mRandomizeVelocityChanceSlider;
-   FloatSlider* mRandomizeVelocityDensitySlider;
-   
-   std::array<double, NSS_MAX_STEPS> mLastStepPlayTime{ -1 };
-   std::array<DropdownList*, NSS_MAX_STEPS> mToneDropdowns;
-   std::array<IntSlider*, NSS_MAX_STEPS> mVelocitySliders;
-   std::array<FloatSlider*, NSS_MAX_STEPS> mLengthSliders;
-   
-   bool mHasExternalPulseSource;
 
-   std::array<AdditionalNoteCable*, NSS_MAX_STEPS> mStepCables;
+   static constexpr int kMaxLength = 32;
+   
+   int mTones[kMaxLength];
+   std::array<double, kMaxLength> mLastColumnPlayTime{ -1 };
+   std::array<int, kMaxLength> mLastColumnNoteOnPitch{ -1 };
+   std::array<DropdownList*, kMaxLength> mToneDropdowns{ nullptr };
+   std::array<AdditionalNoteCable*, kMaxLength> mColumnCables{ nullptr };
 
-   TransportListenerInfo* mTransportListenerInfo;
    GridControlTarget* mGridControlTarget;
    int mGridControlOffsetX;
    int mGridControlOffsetY;
    IntSlider* mGridControlOffsetXSlider;
    IntSlider* mGridControlOffsetYSlider;
 };
-
-
-#endif /* defined(__modularSynth__NoteStepSequencer__) */
-
