7fc974bad5e7e11c6b29cabff521c4b3588109f7
asmw <asmw@asmw.de>
2021-11-05 14:53:12 +0100

    Handle overlapping notes

--- resource/userdata_original/scripts/readmidi.py
+++ resource/userdata_original/scripts/readmidi.py
@@ -1,5 +1,5 @@
 # Path to your midi file
-midifile='/home/asmw/src/BespokeSynth/test.mid'
+midifile='/home/asmw/src/BespokeSynth/overlap.mid'
 
 # If you want to write the notes to a canvas, set its name here
 canvas = ''
@@ -13,11 +13,11 @@ bespoke_notes = []
 
 class BespokeNote(object):
     def __init__(self, pitch, velocity, on_time, off_time):
-        print(on_time, off_time)
         self.pitch = pitch
         self.velocity = velocity
         self.on_time = on_time
         self.off_time = off_time
+        print(self.__str__())
 
     def delay(self):
         return self.on_time
@@ -26,20 +26,28 @@ class BespokeNote(object):
         return self.off_time - self.on_time
     
     def __str__(self):
-        return f'{self.delay()}/{self.pitch}/{self.velocity}/{self.length()}'
+        return f'@{self.delay()}->{self.length()} [{self.pitch}/{self.velocity}]'
 
 timecount = 0.0
 
+on_notes = {}
+
 for msg in mid:
-    if msg.type == 'note_on':
-        last_start = timecount
-    elif msg.type == 'note_off':
-        bespoke_notes.append(BespokeNote(msg.note, msg.velocity, last_start, last_start + msg.time))
+    print(f'{timecount}: {msg}')
     timecount += msg.time
+    if msg.type == 'note_on':
+        on_notes[msg.note] = timecount
+    elif msg.type == 'note_off':
+        if msg.note in on_notes:
+            bespoke_notes.append(BespokeNote(msg.note, msg.velocity, on_notes[msg.note], timecount))
+            del(on_notes[msg.note])
 
 if canvas == '':
     for bn in bespoke_notes:
-        me.schedule_note(bn.delay(), bn.pitch, bn.velocity, bn.length())
+        try:
+            me.schedule_note(bn.delay(), bn.pitch, bn.velocity, bn.length())
+        except:
+            print(bn)
 else:
     import notecanvas
     n = notecanvas.get(canvas)
