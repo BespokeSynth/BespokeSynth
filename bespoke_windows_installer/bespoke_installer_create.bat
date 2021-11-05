e2387a6596adfd2e6cf2711b0017c39f87ac7533
Ryan Challinor <awwbees@gmail.com>
2021-11-04 20:42:32 -0400

    fix up windows installer scripts to new paths
    though they may be soon (or already?) obsolete

--- bespoke_windows_installer/bespoke_installer_create.bat
+++ bespoke_windows_installer/bespoke_installer_create.bat
@@ -1,5 +1,5 @@
-"C:\Program Files (x86)\WiX Toolset v3.11\bin\heat" dir ..\Builds\MacOSX\build\Release\resource -o HarvestedResourceDir.wxs -scom -frag -srd -sreg -gg -cg BespokeResourceDir -dr RESOURCE_DIR_REF
+"C:\Program Files (x86)\WiX Toolset v3.11\bin\heat" dir ..\ignore\build\Source\BespokeSynth_artefacts\Release\resource -o HarvestedResourceDir.wxs -scom -frag -srd -sreg -gg -cg BespokeResourceDir -dr RESOURCE_DIR_REF
 "C:\Program Files (x86)\WiX Toolset v3.11\bin\candle" BespokeSynth.wxs HarvestedResourceDir.wxs -arch x64
-"C:\Program Files (x86)\WiX Toolset v3.11\bin\light" BespokeSynth.wixobj HarvestedResourceDir.wixobj -b ..\Builds\MacOSX\build\Release\resource -out Bespoke-Windows.msi -ext WixUIExtension
+"C:\Program Files (x86)\WiX Toolset v3.11\bin\light" BespokeSynth.wixobj HarvestedResourceDir.wixobj -b ..\ignore\build\Source\BespokeSynth_artefacts\Release\resource -out Bespoke-Windows.msi -ext WixUIExtension
 
 @pause
