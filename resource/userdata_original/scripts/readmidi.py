5be512b20c91a9765de862abf3378aece3fabb7d
asmw <asmw@asmw.de>
2021-11-05 18:35:16 +0100

    Better multitrack and pulse support

--- resource/userdata_original/scripts/readmidi.py
+++ resource/userdata_original/scripts/readmidi.py
@@ -4,8 +4,11 @@ midifile='/home/asmw/src/BespokeSynth/multitrack.mid'
 # If you want to write the notes to a canvas, set its name here
 canvas = ''
 
-# Which track to extract
-track = 1
+# Which tracks to extract/play
+tracks = [1,2]
+
+# whether to play on script load
+play_on_load = False
 
 from mido import MidiFile, tick2second, bpm2tempo
 import time
@@ -16,10 +19,6 @@ try:
     tempo = bpm2tempo(bespoke.get_tempo())
 except:
     pass
-
-mid = MidiFile(midifile)
-
-bespoke_notes = []
 
 class BespokeNote(object):
     def __init__(self, pitch, velocity, on_time, off_time):
@@ -38,37 +37,56 @@ class BespokeNote(object):
     def __str__(self):
         return f'@{self.delay()}->{self.length()} [{self.pitch}/{self.velocity}]'
 
-timecount = 0.0
+def parse_midi(midifile, tracks = [1]):
+    mid = MidiFile(midifile)
+    bespoke_notes = {}
 
-on_notes = {}
+    for track in tracks:
+        if track > len(mid.tracks):
+            continue
 
-if len(mid.tracks) < track:
-    track = 1
+        bespoke_notes[track] = []
+        t = mid.tracks[track - 1]
+        timecount = 0.0
+        on_notes = {}
 
-t = mid.tracks[track - 1]
+        for msg in t:
+            print(f'{timecount}: {msg}')
+            timecount += tick2second(msg.time, mid.ticks_per_beat, tempo)
+            if msg.type == 'note_on':
+                on_notes[msg.note] = timecount
+            elif msg.type == 'note_off':
+                if msg.note in on_notes:
+                    bespoke_notes[track].append(BespokeNote(msg.note, msg.velocity, on_notes[msg.note], timecount))
+                    del(on_notes[msg.note])
+    return bespoke_notes
 
-print(mid.ticks_per_beat)
+nc = None
+if canvas != '':
+    try:
+        import notecanvas
+        nc = notecanvas.get(canvas)
+        nc.clear()
+    except:
+        pass
 
-for msg in t:
-    print(f'{timecount}: {msg}')
-    timecount += tick2second(msg.time, mid.ticks_per_beat, tempo)
-    if msg.type == 'note_on':
-        on_notes[msg.note] = timecount
-    elif msg.type == 'note_off':
-        if msg.note in on_notes:
-            bespoke_notes.append(BespokeNote(msg.note, msg.velocity, on_notes[msg.note], timecount))
-            del(on_notes[msg.note])
+def play(notes, canvas = None):
+    output = 0
+    for track in notes:
+        for bn in notes[track]:
+            if canvas is None:
+                try:
+                    me.schedule_note(bn.delay(), bn.pitch, bn.velocity, bn.length(), 0, output)
+                except:
+                    print(f'{output}: {bn}')
+            else:
+                canvas.add_note(bn.delay(), bn.pitch, bn.velocity, bn.length())
+        output += 1
 
-if canvas == '':
-    for bn in bespoke_notes:
-        try:
-            me.schedule_note(bn.delay(), bn.pitch, bn.velocity, bn.length())
-        except:
-            print(bn)
-else:
-    import notecanvas
-    n = notecanvas.get(canvas)
-    n.clear()
-    for bn in bespoke_notes:
-        n.add_note(bn.delay(), bn.pitch, bn.velocity, bn.length())
-       
+notes = parse_midi(midifile, tracks)
+
+if play_on_load:
+    play(notes)
+
+def on_pulse():
+    play(notes)
