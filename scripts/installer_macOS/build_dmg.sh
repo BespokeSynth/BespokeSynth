6a2cf94ecd0c042bff346ab80c971d1a2dbf50a9
Ryan Challinor <awwbees@gmail.com>
2021-11-06 22:50:31 -0500

    non-working WIP on macOS notarization

--- scripts/installer_macOS/build_dmg.sh
+++ scripts/installer_macOS/build_dmg.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 # requires appdmg, which can be installed via: npm install -g appdmg
-rm -r BespokeSynth
+
+#this copy below is broken, doing a deep copy when I don't want it to. gonna copy by hand for now until I figure out the right way to do it in terminal.
+#rm -rf BespokeSynth
+#cp -r ../../ignore/build/Source/BespokeSynth_artefacts/Release BespokeSynth
+
 rm Bespoke-Mac.dmg
-cp -r Release BespokeSynth
-mkdir BespokeSynth/Bespoke.app/Contents/Frameworks/
-cp Python38 BespokeSynth/Bespoke.app/Contents/Frameworks/
-install_name_tool -change /Library/Frameworks/Python.framework/Versions/3.8/Python @executable_path/../Frameworks/Python38 BespokeSynth/Bespoke.app/Contents/MacOS/Bespoke
-codesign --force --options runtime --timestamp --verbose=4 --entitlements entitlements.plist --sign "Developer ID Application: Ryan Challinor (J5RJ562GN5)" BespokeSynth/Bespoke.app/Contents/MacOS/Bespoke
+codesign --force --options runtime --timestamp --verbose=4 --entitlements entitlements.plist --sign "Developer ID Application: Ryan Challinor (J5RJ562GN5)" BespokeSynth/BespokeSynth.app/Contents/MacOS/BespokeSynth
+codesign --force --options runtime --timestamp --verbose=4 --entitlements entitlements.plist --sign "Developer ID Application: Ryan Challinor (J5RJ562GN5)" BespokeSynth/BespokeSynth.app/Contents/Frameworks/Python.framework/Versions/3.9/Python
+codesign --force --options runtime --timestamp --verbose=4 --entitlements entitlements.plist --sign "Developer ID Application: Ryan Challinor (J5RJ562GN5)" BespokeSynth/BespokeSynth.app/Contents/Frameworks/Python.framework/Versions/3.9/bin/python3.9
+codesign --force --options runtime --timestamp --verbose=4 --entitlements entitlements.plist --sign "Developer ID Application: Ryan Challinor (J5RJ562GN5)" BespokeSynth/BespokeSynth.app/Contents/Frameworks/Python.framework/Versions/3.9/Resources/Python.app/Contents/MacOS/Python
 appdmg bespoke_dmg.json Bespoke-Mac.dmg
 codesign -s "Developer ID Application: Ryan Challinor (J5RJ562GN5)" --timestamp Bespoke-Mac.dmg
