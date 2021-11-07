e4ace13fcabf7e99ba71e20de5f1271967bdcf0e
Ryan Challinor <awwbees@gmail.com>
2021-11-06 23:11:25 -0500

    update DMG packager, wrapping bespoke in a folder is no longer necessary

--- scripts/installer_macOS/build_dmg.sh
+++ scripts/installer_macOS/build_dmg.sh
@@ -6,9 +6,9 @@
 #cp -r ../../ignore/build/Source/BespokeSynth_artefacts/Release BespokeSynth
 
 rm Bespoke-Mac.dmg
-codesign --force --options runtime --timestamp --verbose=4 --entitlements entitlements.plist --sign "Developer ID Application: Ryan Challinor (J5RJ562GN5)" BespokeSynth/BespokeSynth.app/Contents/MacOS/BespokeSynth
-codesign --force --options runtime --timestamp --verbose=4 --entitlements entitlements.plist --sign "Developer ID Application: Ryan Challinor (J5RJ562GN5)" BespokeSynth/BespokeSynth.app/Contents/Frameworks/Python.framework/Versions/3.9/Python
-codesign --force --options runtime --timestamp --verbose=4 --entitlements entitlements.plist --sign "Developer ID Application: Ryan Challinor (J5RJ562GN5)" BespokeSynth/BespokeSynth.app/Contents/Frameworks/Python.framework/Versions/3.9/bin/python3.9
-codesign --force --options runtime --timestamp --verbose=4 --entitlements entitlements.plist --sign "Developer ID Application: Ryan Challinor (J5RJ562GN5)" BespokeSynth/BespokeSynth.app/Contents/Frameworks/Python.framework/Versions/3.9/Resources/Python.app/Contents/MacOS/Python
+codesign --force --options runtime --timestamp --verbose=4 --entitlements entitlements.plist --sign "Developer ID Application: Ryan Challinor (J5RJ562GN5)" BespokeSynth.app/Contents/MacOS/BespokeSynth
+codesign --force --options runtime --timestamp --verbose=4 --entitlements entitlements.plist --sign "Developer ID Application: Ryan Challinor (J5RJ562GN5)" BespokeSynth.app/Contents/Frameworks/Python.framework/Versions/3.9/Python
+codesign --force --options runtime --timestamp --verbose=4 --entitlements entitlements.plist --sign "Developer ID Application: Ryan Challinor (J5RJ562GN5)" BespokeSynth.app/Contents/Frameworks/Python.framework/Versions/3.9/bin/python3.9
+codesign --force --options runtime --timestamp --verbose=4 --entitlements entitlements.plist --sign "Developer ID Application: Ryan Challinor (J5RJ562GN5)" BespokeSynth.app/Contents/Frameworks/Python.framework/Versions/3.9/Resources/Python.app/Contents/MacOS/Python
 appdmg bespoke_dmg.json Bespoke-Mac.dmg
 codesign -s "Developer ID Application: Ryan Challinor (J5RJ562GN5)" --timestamp Bespoke-Mac.dmg
