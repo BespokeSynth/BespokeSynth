BespokeSynth/BespokeSynth

commit 0a1ed8214c4ef6860df8f007ab112bd82cfbf2b0

sort script files in dropdown (#774)

Source/ScriptModule.cpp
@@ -976,11 +976,19 @@ void ScriptModule::RefreshScriptFiles()
 {
    mScriptFilePaths.clear();
    mLoadScriptSelector->Clear();
-   for (const auto& entry : RangedDirectoryIterator{File{ofToDataPath("scripts")}, false, "*.py"})
+   std::list<std::string> scripts;
+   for (const auto& entry : RangedDirectoryIterator{ File{ofToDataPath("scripts")}, false, "*.py" })
    {
       const auto& file = entry.getFile();
-      mLoadScriptSelector->AddLabel(file.getFileName().toStdString(), (int)mScriptFilePaths.size());
-      mScriptFilePaths.push_back(file.getFullPathName().toStdString());
+      scripts.push_back(file.getFileName().toStdString());
+   }
+
+   scripts.sort();
+
+   for (const auto& script : scripts)
+   {
+      mLoadScriptSelector->AddLabel(script, (int)mScriptFilePaths.size());
+      mScriptFilePaths.push_back(script);
    }
 }
 

commit 0d27df4942ad1e1bd4320c829a786bd7d78de226

fix dumb issue caused by #774 where scripts could no longer be loaded
fixes #778

Source/ScriptModule.cpp
@@ -988,7 +988,7 @@ void ScriptModule::RefreshScriptFiles()
    for (const auto& script : scripts)
    {
       mLoadScriptSelector->AddLabel(script, (int)mScriptFilePaths.size());
-      mScriptFilePaths.push_back(script);
+      mScriptFilePaths.push_back(ofToDataPath("scripts/"+script));
    }
 }
 