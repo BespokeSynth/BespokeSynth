e2484f658998b95eb9370a4c9ca27b7d9791ec4e
Ryan Challinor <awwbees@gmail.com>
2022-03-30 00:13:37 -0400

    add noteecho module, to make note delay effect workflow clearer (#802)

---
+++ Source/NoteEcho.h
@@ -0,0 +1,67 @@
+/**
+    bespoke synth, a software modular synthesizer
+    Copyright (C) 2021 Ryan Challinor (contact: awwbees@gmail.com)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
+/*
+  ==============================================================================
+
+    NoteEcho.h
+    Created: 29 March 2022
+    Author:  Ryan Challinor
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <iostream>
+#include "NoteEffectBase.h"
+#include "IDrawableModule.h"
+#include "Checkbox.h"
+#include "INoteSource.h"
+#include "Slider.h"
+
+class NoteEcho : public INoteReceiver, public INoteSource, public IDrawableModule, public IFloatSliderListener
+{
+public:
+   NoteEcho();
+   static IDrawableModule* Create() { return new NoteEcho(); }
+   
+   void CreateUIControls() override;
+   
+   void PlayNote(double time, int pitch, int velocity, int voiceIdx, ModulationParameters modulation) override;
+   void SendCC(int control, int value, int voiceIdx = -1) override;
+   
+   void FloatSliderUpdated(FloatSlider* slider, float oldVal) override {}
+   
+   virtual void LoadLayout(const ofxJSONElement& moduleInfo) override;
+   virtual void SetUpFromSaveData() override;
+   virtual void SaveLayout(ofxJSONElement& moduleInfo) override;
+private:
+   //IDrawableModule
+   void DrawModule() override;
+   void GetModuleDimensions(float& width, float& height) override { width = mWidth; height = mHeight; }
+   bool Enabled() const override { return true; }
+   
+   void SendNoteToIndex(int index, double time, int pitch, int velocity, int voiceIdx, ModulationParameters modulation);
+
+   static const int kMaxDestinations = 5;
+   std::array<float, kMaxDestinations> mDelay;
+   std::array<FloatSlider*, kMaxDestinations> mDelaySlider;
+   std::array<AdditionalNoteCable*, kMaxDestinations> mDestinationCables;
+   float mWidth;
+   float mHeight;
+};
