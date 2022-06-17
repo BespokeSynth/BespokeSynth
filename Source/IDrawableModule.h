BespokeSynth/BespokeSynth

commit a72a5b9805fc665bf7f88289338ccda5f12701f5

don't create sliders for vst parameters until we need them (#864)

Source/VSTPlugin.cpp
@@ -364,15 +364,29 @@ void VSTPlugin::LoadVST(juce::PluginDescription desc)
    mVSTMutex.unlock();
 }
 
+bool VSTPlugin::ParameterNameExists(std::string name, int checkUntilIndex) const
+{
+   for (int i = 0; i < checkUntilIndex; ++i)
+   {
+      if (mParameterSliders[i].mName == name)
+         return true;
+   }
+
+   return false;
+}
+
 void VSTPlugin::CreateParameterSliders()
 {
    assert(mPlugin);
 
    for (auto& slider : mParameterSliders)
    {
-      slider.mSlider->SetShowing(false);
-      RemoveUIControl(slider.mSlider);
-      slider.mSlider->Delete();
+      if (slider.mSlider)
+      {
+         slider.mSlider->SetShowing(false);
+         RemoveUIControl(slider.mSlider);
+         slider.mSlider->Delete();
+      }
    }
    mParameterSliders.clear();
 
@@ -385,26 +399,26 @@ void VSTPlugin::CreateParameterSliders()
    for (int i = 0; i < numParameters; ++i)
    {
       mParameterSliders[i].mValue = parameters[i]->getValue();
-      juce::String name = parameters[i]->getName(32);
-      std::string label(name.getCharPointer());
+      juce::String originalParamName = parameters[i]->getName(32);
+      std::string name(originalParamName.getCharPointer());
       try
       {
          int append = 0;
-         while (FindUIControl(label.c_str()))
+         while (ParameterNameExists(name, i) || FindUIControl(name.c_str()))
          {
             ++append;
-            label = name.toStdString() + ofToString(append);
+            name = originalParamName.toStdString() + ofToString(append);
          }
       }
       catch (UnknownUIControlException& e)
       {
       }
-      mParameterSliders[i].mSlider = new FloatSlider(this, label.c_str(), -1, -1, 200, 15, &mParameterSliders[i].mValue, 0, 1);
       mParameterSliders[i].mParameter = parameters[i];
+      mParameterSliders[i].mName = name.c_str();
       mParameterSliders[i].mShowing = false;
-      if (numParameters <= 20) //only show parameters in list if there are a small number. if there are many, make the user adjust them in the VST before they can be controlled
+      if (numParameters <= 30) //only show parameters in list if there are a small number. if there are many, make the user adjust them in the VST before they can be controlled
       {
-         mShowParameterDropdown->AddLabel(label.c_str(), i);
+         mShowParameterDropdown->AddLabel(name.c_str(), i);
          mParameterSliders[i].mInSelectorList = true;
       }
       else
@@ -434,7 +448,7 @@ void VSTPlugin::Poll()
             if (mTemporarilyDisplayedParamIndex != -1)
                mShowParameterDropdown->RemoveLabel(mTemporarilyDisplayedParamIndex);
             mTemporarilyDisplayedParamIndex = mChangeGestureParameterIndex;
-            mShowParameterDropdown->AddLabel(mParameterSliders[mChangeGestureParameterIndex].mSlider->Name(), mChangeGestureParameterIndex);
+            mShowParameterDropdown->AddLabel(mParameterSliders[mChangeGestureParameterIndex].mName, mChangeGestureParameterIndex);
          }
 
          mChangeGestureParameterIndex = -1;
@@ -845,7 +859,11 @@ void VSTPlugin::DropdownUpdated(DropdownList* list, int oldVal)
             {
                int index = input->readInt();
                if (index < mParameterSliders.size())
+               {
                   mParameterSliders[index].mShowing = true;
+                  if (mParameterSliders[index].mSlider == nullptr)
+                     mParameterSliders[index].MakeSlider(this);
+               }
             }
          }
       }
@@ -854,6 +872,8 @@ void VSTPlugin::DropdownUpdated(DropdownList* list, int oldVal)
    if (list == mShowParameterDropdown)
    {
       mParameterSliders[mShowParameterIndex].mShowing = true;
+      if (mParameterSliders[mShowParameterIndex].mSlider == nullptr)
+         mParameterSliders[mShowParameterIndex].MakeSlider(this);
       mParameterSliders[mShowParameterIndex].mInSelectorList = true;
       mShowParameterIndex = -1;
       mTemporarilyDisplayedParamIndex = -1;
@@ -1016,8 +1036,6 @@ void VSTPlugin::SaveState(FileStreamOut& out)
 {
    out << GetModuleSaveStateRev();
 
-   IDrawableModule::SaveState(out);
-
    if (mPlugin)
    {
       out << true;
@@ -1046,16 +1064,35 @@ void VSTPlugin::SaveState(FileStreamOut& out)
    {
       out << false;
    }
+
+   IDrawableModule::SaveState(out);
 }
 
 void VSTPlugin::LoadState(FileStreamIn& in, int rev)
 {
+   if (rev >= 3)
+   {
+      LoadVSTFromSaveData(in, rev);
+   }
+   else
+   {
+      //make all sliders, like we used to, so that the controls can load correctly
+      for (auto& parameter : mParameterSliders)
+         parameter.MakeSlider(this);
+   }
+
    IDrawableModule::LoadState(in, rev);
 
    if (ModularSynth::sLoadingFileSaveStateRev < 423)
       in >> rev;
    LoadStateValidate(rev <= GetModuleSaveStateRev());
 
+   if (rev < 3)
+      LoadVSTFromSaveData(in, rev);
+}
+
+void VSTPlugin::LoadVSTFromSaveData(FileStreamIn& in, int rev)
+{
    bool hasPlugin;
    in >> hasPlugin;
    if (hasPlugin)
@@ -1095,9 +1132,17 @@ void VSTPlugin::LoadState(FileStreamIn& in, int rev)
          {
             int index;
             in >> index;
-            if (index < mParameterSliders.size())
+            {
                mParameterSliders[index].mShowing = true;
+               if (mParameterSliders[index].mSlider == nullptr)
+                  mParameterSliders[index].MakeSlider(this);
+            }
          }
       }
    }
+}
+
+void VSTPlugin::ParameterSlider::MakeSlider(VSTPlugin* owner)
+{
+   mSlider = new FloatSlider(owner, mName.c_str(), -1, -1, 200, 15, &mValue, 0, 1);
 }

Source/VSTPlugin.h
@@ -95,7 +95,7 @@ public:
    virtual void SetUpFromSaveData() override;
    void SaveState(FileStreamOut& out) override;
    void LoadState(FileStreamIn& in, int rev) override;
-   int GetModuleSaveStateRev() const override { return 2; }
+   int GetModuleSaveStateRev() const override { return 3; }
    std::vector<IUIControl*> ControlsToIgnoreInSaveState() const override;
 
 private:
@@ -105,11 +105,13 @@ private:
    void GetModuleDimensions(float& width, float& height) override;
    bool Enabled() const override { return mEnabled; }
    void LoadVST(juce::PluginDescription desc);
+   void LoadVSTFromSaveData(FileStreamIn& in, int rev);
 
    std::string GetPluginName() const;
    std::string GetPluginId() const;
    void CreateParameterSliders();
    void RefreshPresetFiles();
+   bool ParameterNameExists(std::string name, int checkUntilIndex) const;
 
    //juce::AudioProcessorListener
    void audioProcessorParameterChanged(juce::AudioProcessor* processor, int parameterIndex, float newValue) override {}
@@ -143,6 +145,8 @@ private:
       juce::AudioProcessorParameter* mParameter{ nullptr };
       bool mShowing{ false };
       bool mInSelectorList{ true };
+      std::string mName;
+      void MakeSlider(VSTPlugin* owner);
    };
 
    std::vector<ParameterSlider> mParameterSliders;

commit 0b7bafa75444ab50fa0594dd795fd1491051720b

fix modulators not connecting to VST parameters on load
caused by a72a5b9805fc665bf7f88289338ccda5f12701f5

Source/IDrawableModule.h
@@ -93,6 +93,7 @@ public:
    IUIControl* FindUIControl(const char* name, bool fail = true) const;
    std::vector<IUIControl*> GetUIControls() const;
    std::vector<UIGrid*> GetUIGrids() const;
+   virtual void OnUIControlRequested(const char* name) {}
    void AddChild(IDrawableModule* child);
    void RemoveChild(IDrawableModule* child);
    IDrawableModule* FindChild(const char* name) const;
@@ -195,9 +196,9 @@ public:
    bool mDrawDebug;
 
 protected:
-   virtual void Poll() override {}
-   virtual void OnClicked(int x, int y, bool right) override;
-   virtual bool MouseMoved(float x, float y) override;
+   void Poll() override {}
+   void OnClicked(int x, int y, bool right) override;
+   bool MouseMoved(float x, float y) override;
 
    ModuleSaveData mModuleSaveData;
    Checkbox* mEnabledCheckbox;

Source/ModuleContainer.cpp
@@ -435,6 +435,7 @@ IUIControl* ModuleContainer::FindUIControl(std::string path)
    {
       try
       {
+         module->OnUIControlRequested(control.c_str());
          return module->FindUIControl(control.c_str());
       }
       catch (UnknownUIControlException& e)

Source/VSTPlugin.cpp
@@ -1078,7 +1078,10 @@ void VSTPlugin::LoadState(FileStreamIn& in, int rev)
    {
       //make all sliders, like we used to, so that the controls can load correctly
       for (auto& parameter : mParameterSliders)
-         parameter.MakeSlider(this);
+      {
+         if (parameter.mSlider == nullptr)
+            parameter.MakeSlider(this);
+      }
    }
 
    IDrawableModule::LoadState(in, rev);
@@ -1145,4 +1148,13 @@ void VSTPlugin::LoadVSTFromSaveData(FileStreamIn& in, int rev)
 void VSTPlugin::ParameterSlider::MakeSlider(VSTPlugin* owner)
 {
    mSlider = new FloatSlider(owner, mName.c_str(), -1, -1, 200, 15, &mValue, 0, 1);
+}
+
+void VSTPlugin::OnUIControlRequested(const char* name)
+{
+   for (auto& parameter : mParameterSliders)
+   {
+      if (parameter.mName == name && parameter.mSlider == nullptr)
+         parameter.MakeSlider(this);
+   }
 }

Source/VSTPlugin.h
@@ -91,6 +91,7 @@ public:
    void CheckboxUpdated(Checkbox* checkbox) override;
    void ButtonClicked(ClickButton* button) override;
 
+   void OnUIControlRequested(const char* name) override;
    virtual void LoadLayout(const ofxJSONElement& moduleInfo) override;
    virtual void SetUpFromSaveData() override;
    void SaveState(FileStreamOut& out) override;