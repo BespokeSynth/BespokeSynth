dc3652d08986db25e2e887ea13ebc6b075720ea2
Ryan Challinor <awwbees@gmail.com>
2022-12-22 16:24:41 -0500

    fix crash when saving a sampler at one sample rate and loading the project at a different sample rate (#1021)

--- Source/Sampler.cpp
+++ Source/Sampler.cpp
@@ -320,7 +320,11 @@ void Sampler::LoadState(FileStreamIn& in, int rev)
       in >> rev;
    LoadStateValidate(rev <= GetModuleSaveStateRev());
 
-   in.Read(mSampleData, MAX_SAMPLER_LENGTH);
+   int length = MAX_SAMPLER_LENGTH;
+   if (rev < 2)
+      length = 2 * gSampleRate;
+
+   in.Read(mSampleData, length);
 
    if (rev >= 1)
       in >> mVoiceParams.mSampleLength;
