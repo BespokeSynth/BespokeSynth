4fbbd0ae71f4a2da9b7f29112d692ac568833448
Ryan Challinor <awwbees@gmail.com>
2023-02-08 22:47:18 -0500

    don't light up notefilter when velocity 0 is received
    fixes #1142

--- Source/NoteFilter.cpp
+++ Source/NoteFilter.cpp
@@ -68,7 +68,8 @@ void NoteFilter::PlayNote(double time, int pitch, int velocity, int voiceIdx, Mo
    {
       if (pitch >= 0 && pitch < 128)
       {
-         mLastPlayTime[pitch] = time;
+         if (velocity > 0)
+            mLastPlayTime[pitch] = time;
          if ((pitch >= mMinPitch && pitch <= mMaxPitch && mGate[pitch]) || velocity == 0)
             PlayNoteOutput(time, pitch, velocity, voiceIdx, modulation);
       }
