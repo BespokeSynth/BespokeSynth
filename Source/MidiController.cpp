53f2a006560b3ba3fecacba903bd3d4eb7378809
sbungartz <sbungartz@online.de>
2023-02-19 21:02:00 +0100

    Add grid connections only after layout is loaded
    When a controller layout other than the one named exactly like the midi
    device is used in a saved state, connections on pages that were not
    active when saving are not restored.
    
    This happens because, the grid connections were added in Init.
    At that point, the controller uses the default layout (i.e. the one
    named like the device or the actual default without any grids).
    Afterwards the UI State is loaded in LoadState, and then the selection
    from the Layout dropdown is restored causing mGrids to be (re-)set
    to the correct setup.
    
    Now, we load the grids in LoadState after the UI has restored the
    state of the layout dropdown, so grid connections are always loaded
    into the correct grids.

--- Source/MidiController.cpp
+++ Source/MidiController.cpp
@@ -114,8 +114,11 @@ void MidiController::Init()
    mConnections.clear();
 
    mHasCreatedConnectionUIControls = false;
+   // Only add non-grid connections, because the persisted layout has not been loaded yet.
+   // The grid connections will be added in LoadState.
    for (int i = 0; i < mConnectionsJson.size(); ++i)
-      AddControlConnection(mConnectionsJson[i]);
+      if (!mConnectionsJson[i].isMember("grid_index"))
+         AddControlConnection(mConnectionsJson[i]);
 
    TheTransport->AddAudioPoller(this);
 }
@@ -2418,6 +2421,13 @@ void MidiController::SaveState(FileStreamOut& out)
 void MidiController::LoadState(FileStreamIn& in, int rev)
 {
    IDrawableModule::LoadState(in, rev);
+
+   // Add grid connections here instead of in Init, because the controller
+   // layout file is selected and loaded in IDrawableModule::LoadState,
+   // so the mGrids are only now available to add connections.
+   for (int i = 0; i < mConnectionsJson.size(); ++i)
+      if (mConnectionsJson[i].isMember("grid_index"))
+         AddControlConnection(mConnectionsJson[i]);
 
    if (!ModuleContainer::DoesModuleHaveMoreSaveData(in))
       return; //this was saved before we added versioning, bail out
