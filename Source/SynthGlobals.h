ef3429d76f016a51e8154ce905ebcbba6df31e55
Ryan Challinor <awwbees@gmail.com>
2023-07-15 22:44:44 -0400

    add flag to suppress nightly build labeling (#1378)
    ***NO_CI***

--- Source/SynthGlobals.h
+++ Source/SynthGlobals.h
@@ -52,6 +52,8 @@ void* operator new[](std::size_t size, const char* file, int line) throw(std::ba
 #endif
 #define new DEBUG_NEW
 
+#define BESPOKE_SUPPRESS_NIGHTLY_LABEL 0
+
 #if !defined(__PRETTY_FUNCTION__) && !defined(__GNUC__)
 #define __PRETTY_FUNCTION__ __FUNCSIG__
 #endif
