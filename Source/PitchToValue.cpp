ddea2ce3aabb0bcf26303c0db58c42cc7dc00746
Andrius Merkys <andrius.merkys@gmail.com>
2023-12-19 18:15:40 +0200

    Copy ValueSetter files to become a base for PitchToValue.

---
+++ Source/PitchToValue.cpp
@@ -0,0 +1,118 @@
+/**
+    bespoke synth, a software modular synthesizer
+    Copyright (C) 2021 Ryan Challinor (contact: awwbees@gmail.com)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
+//
+//  ValueSetter.cpp
+//  Bespoke
+//
+//  Created by Ryan Challinor on 1/1/16.
+//
+//
+
+#include "ValueSetter.h"
+#include "OpenFrameworksPort.h"
+#include "Scale.h"
+#include "ModularSynth.h"
+#include "PatchCableSource.h"
+#include "ModulationChain.h"
+#include "UIControlMacros.h"
+
+ValueSetter::ValueSetter()
+: mControlCable(nullptr)
+, mValue(0)
+, mValueEntry(nullptr)
+{
+}
+
+ValueSetter::~ValueSetter()
+{
+}
+
+void ValueSetter::CreateUIControls()
+{
+   IDrawableModule::CreateUIControls();
+   UIBLOCK0();
+   UICONTROL_CUSTOM(mValueEntry, new TextEntry(UICONTROL_BASICS("value"),7,&mValue,-99999,99999); mValueEntry->DrawLabel(true););
+   UIBLOCK_SHIFTRIGHT();
+   UICONTROL_CUSTOM(mButton, new ClickButton(UICONTROL_BASICS("set")));
+   ENDUIBLOCK(mWidth, mHeight);
+   
+   mControlCable = new PatchCableSource(this, kConnectionType_Modulator);
+   AddPatchCableSource(mControlCable);
+}
+
+void ValueSetter::DrawModule()
+{
+   if (Minimized() || IsVisible() == false)
+      return;
+   
+   mValueEntry->Draw();
+   mButton->Draw();
+}
+
+void ValueSetter::PostRepatch(PatchCableSource* cableSource, bool fromUserClick)
+{
+   mTarget = dynamic_cast<IUIControl*>(mControlCable->GetTarget());
+}
+
+void ValueSetter::OnPulse(double time, float velocity, int flags)
+{
+   if (velocity > 0 && mEnabled)
+   {
+      Go();
+   }
+}
+
+void ValueSetter::ButtonClicked(ClickButton* button)
+{
+   if (button == mButton)
+      Go();
+}
+
+void ValueSetter::Go()
+{
+   if (mTarget)
+   {
+      mTarget->SetValue(mValue);
+      mControlCable->AddHistoryEvent(gTime, true);
+      mControlCable->AddHistoryEvent(gTime + 15, false);
+   }
+}
+
+void ValueSetter::SaveLayout(ofxJSONElement& moduleInfo)
+{
+   IDrawableModule::SaveLayout(moduleInfo);
+   
+   std::string targetPath = "";
+   if (mTarget)
+      targetPath = mTarget->Path();
+   
+   moduleInfo["target"] = targetPath;
+}
+
+void ValueSetter::LoadLayout(const ofxJSONElement& moduleInfo)
+{
+   mModuleSaveData.LoadString("target", moduleInfo);
+   
+   SetUpFromSaveData();
+}
+
+void ValueSetter::SetUpFromSaveData()
+{
+   mTarget = TheSynth->FindUIControl(mModuleSaveData.GetString("target"));
+   mControlCable->SetTarget(mTarget);
+}
---
+++ Source/PitchToValue.h
@@ -0,0 +1,80 @@
+/**
+    bespoke synth, a software modular synthesizer
+    Copyright (C) 2021 Ryan Challinor (contact: awwbees@gmail.com)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
+//
+//  ValueSetter.h
+//  Bespoke
+//
+//  Created by Ryan Challinor on 1/1/16.
+//
+//
+
+#ifndef __Bespoke__ValueSetter__
+#define __Bespoke__ValueSetter__
+
+#include "IDrawableModule.h"
+#include "IPulseReceiver.h"
+#include "TextEntry.h"
+#include "ClickButton.h"
+
+class PatchCableSource;
+class IUIControl;
+
+class ValueSetter : public IDrawableModule, public IPulseReceiver, public ITextEntryListener, public IButtonListener
+{
+public:
+   ValueSetter();
+   virtual ~ValueSetter();
+   static IDrawableModule* Create() { return new ValueSetter(); }
+   
+   
+   void CreateUIControls() override;
+   
+   void SetEnabled(bool enabled) override { mEnabled = enabled; }
+   
+   //IPulseReceiver
+   void OnPulse(double time, float velocity, int flags) override;
+   
+   void ButtonClicked(ClickButton* button) override;
+   
+   //IPatchable
+   void PostRepatch(PatchCableSource* cableSource, bool fromUserClick) override;
+   
+   void TextEntryComplete(TextEntry* entry) override {}
+   
+   void SaveLayout(ofxJSONElement& moduleInfo) override;
+   void LoadLayout(const ofxJSONElement& moduleInfo) override;
+   void SetUpFromSaveData() override;
+private:
+   //IDrawableModule
+   void DrawModule() override;
+   void GetModuleDimensions(float& width, float& height) override { width = mWidth; height = mHeight; }
+   bool Enabled() const override { return mEnabled; }
+   
+   void Go();
+   
+   PatchCableSource* mControlCable;
+   IUIControl* mTarget;
+   float mValue;
+   TextEntry* mValueEntry;
+   ClickButton* mButton;
+   
+   float mWidth;
+   float mHeight;
+};
+
+#endif /* defined(__Bespoke__ValueSetter__) */
