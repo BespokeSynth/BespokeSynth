987f6c5b01bb2ad4ec8254632efe914809200938
Andrius Merkys <andrius.merkys@gmail.com>
2023-12-22 08:46:50 +0200

    Copy Source/NoteMin.* to Source/NoteMinMax.* to develop a module to send both the min and max notes.

---
+++ Source/NoteMinMax.cpp
@@ -0,0 +1,88 @@
+/**
+    bespoke synth, a software modular synthesizer
+    Copyright (C) 2021 Ryan Challinor (contact: awwbees@gmail.com)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
+/*
+  ==============================================================================
+
+    ChordHold.cpp
+    Created: 3 Mar 2021 9:56:09pm
+    Author:  Ryan Challinor
+
+  ==============================================================================
+*/
+
+#include "NoteMinMax.h"
+#include "OpenFrameworksPort.h"
+#include "ModularSynth.h"
+
+NoteMinMax::NoteMinMax()
+{
+}
+
+void NoteMinMax::CreateUIControls()
+{
+   IDrawableModule::CreateUIControls();
+}
+
+void NoteMinMax::DrawModule()
+{
+   if (Minimized() || IsVisible() == false)
+      return;
+}
+
+void NoteMinMax::PlayNote(double time, int pitch, int velocity, int voiceIdx, ModulationParameters modulation)
+{
+   if (mEnabled)
+   {
+      mNotePlaying[pitch] = velocity > 0;
+      mVelocityPlaying[pitch] = velocity;
+      mVoiceIdxPlaying[pitch] = voiceIdx;
+
+      int minNotePlaying = -1;
+      for (int i = 0; i < 128; ++i)
+      {
+         if (mNotePlaying[i] && minNotePlaying == -1)
+            minNotePlaying = i;
+      }
+
+      if (velocity > 0) // new note playing
+      {
+          if (minNotePlaying == pitch)
+             PlayNoteOutput(time, minNotePlaying, mVelocityPlaying[minNotePlaying], mVoiceIdxPlaying[minNotePlaying], modulation);
+      } else { // played note is stopped
+          PlayNoteOutput(time, pitch, 0, -1);
+          if (minNotePlaying > pitch) // play the new lowest note
+             PlayNoteOutput(time, minNotePlaying, mVelocityPlaying[minNotePlaying], mVoiceIdxPlaying[minNotePlaying], modulation);
+      }
+   }
+   else
+   {
+      PlayNoteOutput(time, pitch, velocity, voiceIdx, modulation);
+   }
+}
+
+void NoteMinMax::LoadLayout(const ofxJSONElement& moduleInfo)
+{
+   mModuleSaveData.LoadString("target", moduleInfo);
+
+   SetUpFromSaveData();
+}
+
+void NoteMinMax::SetUpFromSaveData()
+{
+   SetUpPatchCables(mModuleSaveData.GetString("target"));
+}
---
+++ Source/NoteMinMax.h
@@ -0,0 +1,59 @@
+/**
+    bespoke synth, a software modular synthesizer
+    Copyright (C) 2021 Ryan Challinor (contact: awwbees@gmail.com)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
+/*
+  ==============================================================================
+
+    ChordHold.h
+    Created: 3 Mar 2021 9:56:09pm
+    Author:  Ryan Challinor
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <iostream>
+#include "NoteEffectBase.h"
+#include "IDrawableModule.h"
+
+class NoteMinMax : public NoteEffectBase, public IDrawableModule
+{
+public:
+   NoteMinMax();
+   static IDrawableModule* Create() { return new NoteMinMax(); }
+
+   
+   void CreateUIControls() override;
+
+   void SetEnabled(bool enabled) override { mEnabled = enabled; }
+
+   //INoteReceiver
+   void PlayNote(double time, int pitch, int velocity, int voiceIdx = -1, ModulationParameters modulation = ModulationParameters()) override;
+
+   virtual void LoadLayout(const ofxJSONElement& moduleInfo) override;
+   virtual void SetUpFromSaveData() override;
+private:
+   //IDrawableModule
+   void DrawModule() override;
+   void GetModuleDimensions(float& width, float& height) override { width = 90; height = 0; }
+   bool Enabled() const override { return mEnabled; }
+
+   std::array<bool, 128> mNotePlaying{ false };
+   std::array<int, 128> mVelocityPlaying{ 0 };
+   std::array<int, 128> mVoiceIdxPlaying{ 0 };
+};
