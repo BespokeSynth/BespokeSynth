8056244cbbb62d1c2bf4d5253c3a0525547f0d65
Afentoe <jack@afentoe.com>
2024-12-15 00:47:12 +0100

    Added send_sysex setting to make MidiController backwords compatible and avoid unhandled on_sysex events in existing python scripts

--- Source/MidiController.cpp
+++ Source/MidiController.cpp
@@ -485,7 +485,7 @@ void MidiController::OnMidi(const MidiMessage& message)
    int is_sysex = message.isSysEx();
    if (!mEnabled || (mChannelFilter != ChannelFilter::kAny && message.getChannel() != (int)mChannelFilter && !is_sysex))
       return;
-   if (is_sysex)
+   if (mEnabled && mSendSysex && is_sysex)
       for (auto* script : mScriptListeners)
          script->SysExReceived(message.getSysExData(), message.getSysExDataSize());
 
@@ -2420,6 +2420,7 @@ void MidiController::LoadLayout(const ofxJSONElement& moduleInfo)
    mModuleSaveData.LoadBool("twoway_on_change", moduleInfo, true);
    mModuleSaveData.LoadBool("resend_feedback_on_release", moduleInfo, false);
    mModuleSaveData.LoadBool("show_activity_ui_overlay", moduleInfo, true);
+   mModuleSaveData.LoadBool("send_sysex", moduleInfo, false);
 
    mConnectionsJson = moduleInfo["connections"];
 
@@ -2452,6 +2453,7 @@ void MidiController::SetUpFromSaveData()
    mSendTwoWayOnChange = mModuleSaveData.GetBool("twoway_on_change");
    mResendFeedbackOnRelease = mModuleSaveData.GetBool("resend_feedback_on_release");
    mShowActivityUIOverlay = mModuleSaveData.GetBool("show_activity_ui_overlay");
+   mSendSysex = mModuleSaveData.GetBool("send_sysex");
 
    BuildControllerList();
 
