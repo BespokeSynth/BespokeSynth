c6c17a91e636b24011084c27fd16c354cca29a67
Ryan Challinor <awwbees@gmail.com>
2026-03-21 15:56:44 -0400

    fix warning introduced in #1980

--- Source/AudioRouter.cpp
+++ Source/AudioRouter.cpp
@@ -141,8 +141,7 @@ void AudioRouter::RadioButtonUpdated(RadioButton* radio, int oldVal, double time
 {
    if (radio == mRouteSelector)
    {
-      if (mRouteIndex >= mDestinationCables.size())
-         mRouteIndex = mDestinationCables.size() - 1;
+      mRouteIndex = ofClamp(mRouteIndex, 0, (int)mDestinationCables.size() - 1);
    }
 }
 
