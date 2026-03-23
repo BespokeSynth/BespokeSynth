6e74aa7656147294901579d750be0e1de3c0577c
Ryan Challinor <awwbees@gmail.com>
2026-03-22 21:50:04 -0400

    add the ability to python script custom interfaces for ableton push and move controllers (#1987)

---
+++ resource/python_stubs/abletongriddevice/__init__.pyi
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+def get_current():
+   pass
+
+class abletongriddevice:
+   def set_led(this, index, color, flashColor = -1 -1):
+      pass
+
+   def get_button_state(this, buttonIndex):
+      pass
+
+   def display_screen_message(this, message, durationMs = 500 500):
+      pass
+
