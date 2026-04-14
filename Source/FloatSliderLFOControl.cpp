BespokeSynth/BespokeSynth

commit 4bdf3113b485e38cc2aaa42f1c9730da245c43d4

`abletonmove`: added the ability to record knob automation and add LFOs (#2002)
***NO_CI***

Source/FloatSliderLFOControl.cpp
@@ -179,6 +179,14 @@ void FloatSliderLFOControl::DrawModule()
    }
    ofEndShape(false);
 
+   float xNormalized;
+   float yNormalized;
+   GetCurrentPhaseDrawPosition(xNormalized, yNormalized);
+   ofCircle(xNormalized * width + x, yNormalized * height + y, 2);
+}
+
+void FloatSliderLFOControl::GetCurrentPhaseDrawPosition(float& xNormalized, float& yNormalized)
+{
    float currentPhase = mLFO.CalculatePhase(0, false);
    float squeeze;
    if (mLFO.GetOsc()->GetShuffle() == 0)
@@ -199,8 +207,51 @@ void FloatSliderLFOControl::DrawModule()
       currentPhase = 0;
       displayPhase = 0;
    }
-   ofCircle(displayPhase / squeeze * width + x,
-            ofMap(GetLFOValue(0, mLFO.TransformPhase(currentPhase)), GetTargetMax(), GetTargetMin(), 0, height) + y, 2);
+
+   xNormalized = displayPhase / squeeze;
+   yNormalized = ofLerp(GetTargetMax(), GetTargetMin(), GetLFOValue(0, mLFO.TransformPhase(currentPhase)));
+}
+
+void FloatSliderLFOControl::DrawToAbletonMoveScreen(AbletonMoveLCD* screen)
+{
+   int lastY = -1;
+   for (float x = 0; x < AbletonMoveLCD::kMoveDisplayWidth; ++x)
+   {
+      float phase = x / AbletonMoveLCD::kMoveDisplayWidth;
+      if (mLFO.GetOsc()->GetShuffle() > 0)
+         phase *= 2;
+      if (mLFO.GetOsc()->GetType() != kOsc_Perlin)
+         phase += 1 - mLFOSettings.mLFOOffset;
+      float value = GetLFOValue(0, mLFO.TransformPhase(phase));
+      int newY = int(ofMap(value,
+                           GetTargetMax(), GetTargetMin(),
+                           10, AbletonMoveLCD::kMoveDisplayHeight - 10));
+
+      if (lastY == -1)
+         lastY = newY;
+      if (lastY <= newY)
+      {
+         for (int y = lastY; y <= newY; ++y)
+            screen->TogglePixel(x, y);
+      }
+      else
+      {
+         for (int y = lastY; y >= newY; --y)
+            screen->TogglePixel(x, y);
+      }
+
+      lastY = newY;
+   }
+
+   if (Active())
+   {
+      float xNormalized;
+      float yNormalized;
+      GetCurrentPhaseDrawPosition(xNormalized, yNormalized);
+
+      screen->DrawRect(ofLerp(0, AbletonMoveLCD::kMoveDisplayWidth, xNormalized),
+                       ofLerp(10, AbletonMoveLCD::kMoveDisplayHeight - 10, yNormalized), 3, 3, false);
+   }
 }
 
 bool FloatSliderLFOControl::DrawToPush2Screen()

Source/AbletonDeviceShared.h
@@ -492,10 +492,16 @@ public:
 class IAbletonGridController
 {
 public:
+   enum class LCDDrawPass
+   {
+      HighPriority,
+      Normal,
+      Overlay
+   };
+
    virtual ~IAbletonGridController() {}
    virtual void OnAbletonGridConnect(IAbletonGridDevice* abletonGrid) {}
    virtual bool OnAbletonGridControl(IAbletonGridDevice* abletonGrid, int controlIndex, float midiValue) = 0;
    virtual void UpdateAbletonGridLeds(IAbletonGridDevice* abletonGrid) = 0;
-   virtual bool UpdateAbletonMoveScreen(IAbletonGridDevice* abletonGrid, AbletonMoveLCD* lcd) { return false; }
-   virtual bool HasHighPriorityAbletonMoveScreenUpdate(IAbletonGridDevice* abletonGrid) { return false; }
+   virtual bool UpdateAbletonMoveScreen(IAbletonGridDevice* abletonGrid, AbletonMoveLCD* lcd, LCDDrawPass drawPass) { return false; }
 };

Source/AbletonMoveControl.h
@@ -133,6 +133,8 @@ private:
    void ZoomToTrack(TrackOrganizer* track);
    void OnTrackRowExited(int newRow, int oldRow);
    void ShowSoundSelector();
+   void StartControlRecorder(int controlIndex);
+   void DrawKnobDisplay(int controlIndex);
 
    void OnMidiNote_Consume(MidiNote& note);
    void OnMidiControl_Consume(MidiControl& control);
@@ -153,6 +155,7 @@ private:
 
    IDrawableModule* mDisplayModule{ nullptr };
    std::string mDisplayModuleContext{};
+   ControlRecorder* mCurrentControlRecorder{ nullptr };
    std::vector<IUIControl*> mControls;
    bool mDisplayModuleIsShowingOverrideControls{ false };
 

Source/ControlRecorder.cpp
@@ -90,7 +90,7 @@ void ControlRecorder::Poll()
       RecordPoint();
 }
 
-float ControlRecorder::GetPlaybackTime(double time)
+float ControlRecorder::GetPlaybackTime(double time) const
 {
    float measureTime = TheTransport->GetMeasureTime(time) - mRecordStartOffset;
    if (!mQuantizeLength)

Source/ControlRecorder.h
@@ -52,6 +52,8 @@ public:
 
    void SetRecording(bool on);
    float GetLength() const { return mLength; }
+   float GetValue(float percent) { return mCurve.EvaluateAtPercent(percent); }
+   float GetPlaybackTime(double time) const;
 
    //IDrawableModule
    void Init() override;
@@ -84,7 +86,6 @@ public:
    bool IsEnabled() const override { return mEnabled; }
 
 private:
-   float GetPlaybackTime(double time);
    void RecordPoint();
    void Clear();
 

Source/Curve.cpp
@@ -62,7 +62,7 @@ void Curve::AddPointAtEnd(CurvePoint point)
    ++mNumCurvePoints;
 }
 
-int Curve::FindIndexForTime(float time)
+int Curve::FindIndexForTime(float time) const
 {
    int max = mNumCurvePoints - 1;
    int left = 0;
@@ -83,6 +83,11 @@ int Curve::FindIndexForTime(float time)
    return -1;
 }
 
+float Curve::EvaluateAtPercent(float percent, bool holdEndForLoop)
+{
+   return Evaluate(ofLerp(mStart, mEnd, percent), holdEndForLoop);
+}
+
 float Curve::Evaluate(float time, bool holdEndForLoop)
 {
    float retVal = mDefaultValue;
@@ -99,7 +104,7 @@ float Curve::Evaluate(float time, bool holdEndForLoop)
 
       int beforeIndex = 0;
       int quickCheckIndex = mLastEvalIndex;
-      if (quickCheckIndex < mNumCurvePoints &&
+      if (quickCheckIndex >= 0 && quickCheckIndex < mNumCurvePoints &&
           mPoints[quickCheckIndex].mTime < time &&
           (quickCheckIndex == mNumCurvePoints - 1 || mPoints[quickCheckIndex + 1].mTime >= time))
       {
@@ -136,7 +141,7 @@ void Curve::Render()
    ofBeginShape();
    for (int i = 0; i < mWidth; ++i)
    {
-      float val = Evaluate(ofMap(float(i) / mWidth, 0, 1, mStart, mEnd));
+      float val = EvaluateAtPercent(float(i) / mWidth);
 
       if (i > 0)
       {

Source/Curve.h
@@ -50,6 +50,7 @@ public:
    void AddPoint(CurvePoint point);
    void AddPointAtEnd(CurvePoint point); //only use this if you are sure that there are no points already added at an earlier time
    float Evaluate(float time, bool holdEndForLoop = false);
+   float EvaluateAtPercent(float percent, bool holdEndForLoop = false);
    void Render() override;
    void SetExtents(float start, float end)
    {
@@ -81,7 +82,7 @@ protected:
 
 private:
    bool IsAtCapacity() { return mNumCurvePoints >= (int)mPoints.size(); }
-   int FindIndexForTime(float time);
+   int FindIndexForTime(float time) const;
    std::array<CurvePoint, 5000> mPoints;
    int mNumCurvePoints{ 0 };
    float mWidth{ 200 };

commit b2284cc70758a94b92de3a9d1747a322069b98bc

fix LFO display being broken by #2002 when a slider's range is not 0-1

Source/FloatSliderLFOControl.cpp
@@ -209,7 +209,7 @@ void FloatSliderLFOControl::GetCurrentPhaseDrawPosition(float& xNormalized, floa
    }
 
    xNormalized = displayPhase / squeeze;
-   yNormalized = ofLerp(GetTargetMax(), GetTargetMin(), GetLFOValue(0, mLFO.TransformPhase(currentPhase)));
+   yNormalized = ofMap(GetLFOValue(0, mLFO.TransformPhase(currentPhase)), GetTargetMax(), GetTargetMin(), 0, 1);
 }
 
 void FloatSliderLFOControl::DrawToAbletonMoveScreen(AbletonMoveLCD* screen)