50436076e518842a2216c17006b1930ebcd2f925
ArkyonVeil <arkyonveil@gmail.com>
2026-04-17 23:11:25 +0100

    - Sampler now supports the following pulse Flags: - Reset --> Stops all sound - Repeat --> Interrupts the previous note and plays it on the same channel. - Random --> Random plays the sample with a random offset. Handy for a granular play.

--- Source/Sampler.cpp
+++ Source/Sampler.cpp
@@ -271,7 +271,34 @@ void Sampler::OnPulse(double time, float velocity, int flags)
    //   note.velocity = velocity;
    note.velocity = 127;
    note.pitch = 48;
+
+   int repeatFlagInfo = -1;
+   if (flags & kPulseFlag_Reset) //Stops all sound.
+   {
+      mPolyMgr.KillAll();
+      note.velocity = 0;
+   }
+   else if (flags & kPulseFlag_Random) //Plays the sample at a random pos
+   {
+      repeatFlagInfo = mVoiceParams.mStartSample;
+      int range = mVoiceParams.mStopSample - mVoiceParams.mStartSample;
+      mVoiceParams.mStartSample = mVoiceParams.mStartSample + (gRandom() % range);
+   }
+   else if (flags & kPulseFlag_Repeat) //Replays on the last voice channel.
+   {
+      note.voiceIdx = mMostRecentVoiceIdx;
+   }
+   else if (flags & kPulseFlag_Backward)
+   {
+      //Obvious, but not supported. Pending polyphonic player/SampleVoice support.
+   }
+
    PlayNote(note);
+
+   if (repeatFlagInfo != -1)
+   {
+      mVoiceParams.mStartSample = repeatFlagInfo;
+   }
 }
 
 void Sampler::FilesDropped(std::vector<std::string> files, int x, int y)
