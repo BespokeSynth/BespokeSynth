e51aa8a3192ab1c82a518ada5b197d2fd78235a4
Simon Bungartz <simon.bungartz@zweitag.de>
2026-06-04 06:39:53 +0200

    Disconnect controls when midicontroller is disabled (#1488)

--- Source/MidiController.cpp
+++ Source/MidiController.cpp
@@ -1559,7 +1559,7 @@ void MidiController::GetModuleDimensions(float& width, float& height)
 
 void MidiController::ResyncControllerState()
 {
-   if (mControllerPage >= 0 && mControllerPage < mListeners.size())
+   if (mEnabled && mControllerPage >= 0 && mControllerPage < mListeners.size())
    {
       for (auto i = mListeners[mControllerPage].begin(); i != mListeners[mControllerPage].end(); ++i)
          (*i)->ControllerPageSelected();
@@ -1590,7 +1590,7 @@ void MidiController::ResyncControllerState()
    }
    for (auto* grid : mGrids)
    {
-      if (grid->mGridControlTarget[mControllerPage] != nullptr)
+      if (mEnabled && grid->mGridControlTarget[mControllerPage] != nullptr)
       {
          //reset target
          GridControlTarget* target = grid->mGridControlTarget[mControllerPage];
@@ -1602,11 +1602,15 @@ void MidiController::ResyncControllerState()
          grid->mGridCable->ClearPatchCables();
       }
    }
-   HighlightPageControls(mControllerPage);
 
-   for (auto i = mConnections.begin(); i != mConnections.end(); ++i)
+   if (mEnabled)
    {
-      (*i)->mLastControlValue = -1;
+      HighlightPageControls(mControllerPage);
+
+      for (auto i = mConnections.begin(); i != mConnections.end(); ++i)
+      {
+         (*i)->mLastControlValue = -1;
+      }
    }
 }
 
@@ -1979,6 +1983,16 @@ void MidiController::OnDeviceChanged()
 
 void MidiController::CheckboxUpdated(Checkbox* checkbox, double time)
 {
+   if (checkbox == GetEnabledCheckbox())
+   {
+      if (!mEnabled)
+      {
+         SetEntirePageToZero(mControllerPage);
+      }
+      ResyncControllerState();
+      return;
+   }
+
    for (auto iter = mConnections.begin(); iter != mConnections.end(); ++iter)
    {
       UIControlConnection* connection = *iter;
@@ -2159,7 +2173,7 @@ void MidiController::PostRepatch(PatchCableSource* cableSource, bool fromUserCli
 {
    for (auto* grid : mGrids)
    {
-      if (cableSource == grid->mGridCable)
+      if (mEnabled && cableSource == grid->mGridCable)
       {
          grid->mGridControlTarget[mControllerPage] = dynamic_cast<GridControlTarget*>(cableSource->GetTarget());
          if (grid->mGridControlTarget[mControllerPage])
